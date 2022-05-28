a55c17288fa58135bceb32a314cd6241b11baa11
gcharkiewicz <gcharkiewicz@enzode.com>
2022-05-28 12:58:05 +0200

    total_ordering

--- OOP/dunders.py
+++ OOP/dunders.py
@@ -1,5 +1,6 @@
 from copy import copy
 
+
 class Book:
     def __init__(self, title, author, book_type, pages):
         self.title = title
@@ -62,6 +63,7 @@ print(a == z)
 print(id(a))
 print(id(z))
 
+
 # Mutowalne struktury danych
 
 class Tire:
@@ -81,17 +83,17 @@ class BMW:
 
 
 bmw1 = BMW()
-print(bmw1.tires) # 4 różne instancje Tire
+print(bmw1.tires)  # 4 różne instancje Tire
 for tire in bmw1.tires:
     """Funkcja id() Pythona zwraca „tożsamość” obiektu. Tożsamość obiektu jest liczbą całkowitą, 
     która gwarantuje, że jest unikalna i stała dla tego obiektu podczas jego życia."""
     print(id(tire))
 bmw2 = BMW()
-print(bmw2.tires) # 4 różne instancje Tire
+print(bmw2.tires)  # 4 różne instancje Tire
 for tire2 in bmw2.tires:
     """Funkcja id() Pythona zwraca „tożsamość” obiektu. Tożsamość obiektu jest liczbą całkowitą, 
     która gwarantuje, że jest unikalna i stała dla tego obiektu podczas jego życia."""
-    print(id(tire2)) # Te samo id jak w instancji bmw1 !
+    print(id(tire2))  # Te samo id jak w instancji bmw1 !
 
 print("===================================================================\n")
 print("=================== DODANIE OBIEKTU DO LISTY ======================\n")
@@ -104,8 +106,8 @@ for tire in bmw1.tires:
 print("================================ BMW2 ================================\n")
 
 for tire2 in bmw2.tires:
-    print(id(tire2)) # Pomimo dodania do instanji bmw1 nowego objektu, on również jest dostępny w bmw2
-
+    print(
+        id(tire2))  # Pomimo dodania do instanji bmw1 nowego objektu, on również jest dostępny w bmw2
 
 
 # Hashable Book
@@ -130,6 +132,21 @@ class Book:
 
         return self.title == other.title and self.author == other.author
 
+    def __gt__(self, other):
+        if not isinstance(other, Book):
+            return NotImplemented
+
+        return self.pages > other.pages
+
+    def __lt__(self, other):
+        return NotImplemented
+
+    def __len__(self):
+        return NotImplemented
+
+    def __ge__(self, other):
+        return NotImplemented
+
     def __hash__(self):
         return hash((self.title, self.author))
 
@@ -143,3 +160,49 @@ print(id(a))
 print(id(z))
 print(hash(z))
 print(hash(z))
+
+from functools import total_ordering
+
+
+@total_ordering
+class Book:
+    """
+    Object Equality
+    """
+
+    def __init__(self, title, author, book_type, pages):
+        self.title = title
+        self.author = author
+        self.book_type = book_type
+        self.pages = pages
+
+    def __repr__(self):
+        """Information more dev/code user oriented"""
+        return f"Book('{self.title}', '{self.author}', '{self.book_type}', '{self.pages}')"
+
+    def __eq__(self, other):
+        if not isinstance(other, Book):
+            return False
+
+        return self.title == other.title and self.author == other.author
+
+    def __gt__(self, other):
+        if not isinstance(other, Book):
+            return NotImplemented
+
+        return self.pages > other.pages
+
+    def __hash__(self):
+        return hash((self.title, self.author))
+
+
+# Book = total_ordering(Book) or add @total_ordering
+a = Book("Antifragile", "Nassim Taleb", "Hardcover", 519)
+z = Book("How Asia", "Nassim Taleb", "Hardcover", 472)
+
+print(a == z)
+print(a != z)
+print(a < z)
+print(a <= z)
+print(a > z)
+print(a >= z)
